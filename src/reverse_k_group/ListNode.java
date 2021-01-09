37bc36ad27e7258a66e5f4d71ad0b356216eebf6
姜兴琪 <xingqijiang@qq.com>
2021-01-09 21:13:14 +0800

    25. K 个一组翻转链表

---
+++ src/reverse_k_group/ListNode.java
@@ -0,0 +1,9 @@
+package reverse_k_group;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
