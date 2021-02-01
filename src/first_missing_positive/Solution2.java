90833eaa2d675d29681dadc20f8f2b06fa1dc74f
姜兴琪 <xingqijiang@qq.com>
2021-02-01 10:21:42 +0800

    Leetcode No.41 缺失的第一个正数

---
+++ src/first_missing_positive/Solution2.java
@@ -0,0 +1,17 @@
+package first_missing_positive;
+
+import java.util.HashMap;
+
+public class Solution2 {
+    public int firstMissingPositive(int[] nums) {
+        HashMap map=new HashMap<Integer,Integer>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],1);
+        }
+        int i=1;
+        while(map.get(i)!=null){
+            i++;
+        }
+        return i;
+    }
+}
