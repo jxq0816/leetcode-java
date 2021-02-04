15a3f07129be183a1bd52bae24c5403d00d812e9
姜兴琪 <xingqijiang@qq.com>
2021-02-04 18:58:56 +0800

    Leetcode No.47 全排列2

---
+++ src/permute_unique/Solution.java
@@ -0,0 +1,45 @@
+package permute_unique;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    boolean[] vis;
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        List<Integer> perm = new ArrayList<Integer>();
+        vis = new boolean[nums.length];
+        Arrays.sort(nums);
+        backtrack(nums, ans, 0, perm);
+        return ans;
+    }
+    public void backtrack(int[] nums, List<List<Integer>> ans, int idx, List<Integer> perm) {
+        if (idx == nums.length) {
+            ans.add(new ArrayList<Integer>(perm));
+            return;
+        }
+        for (int i = 0; i < nums.length; ++i) {
+            if (vis[i] || (i > 0 && nums[i] == nums[i - 1] && !vis[i - 1])) {
+                continue;
+            }
+            perm.add(nums[i]);
+            vis[i] = true;
+            backtrack(nums, ans, idx + 1, perm);
+            vis[i] = false;
+            perm.remove(idx);
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution=new Solution();
+        int[] array={1,1,2};
+        List<List<Integer>> rs=solution.permuteUnique(array);
+        for(List<Integer> list:rs){
+            for(Integer i:list){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
