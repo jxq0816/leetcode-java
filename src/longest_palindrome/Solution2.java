64b7999750f5f150d2016b688b18ca9a0ddb4019
姜兴琪 <xingqijiang@qq.com>
2021-02-14 00:31:01 +0800

    No.5 最长回文子串

--- src/longest_palindrome/Solution2.java
+++ src/longest_palindrome/Solution2.java
@@ -4,41 +4,28 @@ public class Solution2 {
 
     public String longestPalindrome(String s) {
         // 特判
-        int len = s.length();
-        if (len < 2) {
-            return s;
-        }
-
-        int maxLen = 1;
-        int begin = 0;
-
+        int n = s.length();
         // dp[i][j] 表示 s[i, j] 是否是回文串
-        boolean[][] dp = new boolean[len][len];
-        char[] charArray = s.toCharArray();
-
-        for (int i = 0; i < len; i++) {
-            dp[i][i] = true;
-        }
-        for (int j = 1; j < len; j++) {
-            for (int i = 0; i < j; i++) {
-                if (charArray[i] != charArray[j]) {
-                    dp[i][j] = false;
-                } else {
-                    if (j - i < 3) {
-                        dp[i][j] = true;
-                    } else {
-                        dp[i][j] = dp[i + 1][j - 1];
-                    }
+        boolean[][] dp = new boolean[n][n];
+        String rs="";
+        for (int k = 0;  k< n; ++k) {
+            for (int i = 0; i+k < n; i++) {
+                int j=i+k;
+                System.out.println(j);
+                if(k==0){
+                    dp[i][j]=true;
+                }else if(k==1){
+                    dp[i][j]=(s.charAt(i)==s.charAt(j));
+                }else{
+                    dp[i][j]=(s.charAt(i)==s.charAt(j))&&dp[i+1][j-1];
                 }
-
                 // 只要 dp[i][j] == true 成立，就表示子串 s[i..j] 是回文，此时记录回文长度和起始位置
-                if (dp[i][j] && j - i + 1 > maxLen) {
-                    maxLen = j - i + 1;
-                    begin = i;
+                if (dp[i][j] && k + 1 > rs.length()) {
+                    rs = s.substring(i,i+k+1);
                 }
             }
         }
-        return s.substring(begin, begin + maxLen);
+        return rs;
     }
 
     public static void main(String[] args) {
