3f2195215a40156bbb6a1a2cb60b1538a3db53ed
姜兴琪 <xingqijiang@qq.com>
2021-08-11 01:02:22 +0800

    No.102 二叉树的层序遍历

---
+++ src/levelOrder/Solution.java
@@ -0,0 +1,34 @@
+package levelOrder;
+
+import utils.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> rs = new ArrayList<>();
+        Queue<TreeNode> queue=new ArrayDeque<>();
+        if(root!=null){
+            queue.add(root);
+        }
+        while(!queue.isEmpty()){
+            int n=queue.size();
+            List<Integer> level = new ArrayList<>();
+            for(int i=0;i<n;i++){
+                TreeNode node=queue.poll();
+                level.add(node.val);
+                if(node.left != null){
+                    queue.add(node.left);
+                }
+                if(node.right != null){
+                    queue.add(node.right);
+                }
+            }
+            rs.add(level);
+        }
+        return rs;
+    }
+}
