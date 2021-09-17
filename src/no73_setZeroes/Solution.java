2e3bdf00be0c80d6e7b8fc9553326cd0b2a92321
姜兴琪 <xingqijiang@qq.com>
2021-09-17 23:30:59 +0800

    73. 矩阵置零

--- src/no73_setZeroes/Solution.java
+++ src/no73_setZeroes/Solution.java
@@ -36,8 +36,9 @@ class Solution {
         int[][] matrix={{0,1,2,0},{3,4,5,2},{1,3,1,5}};
         Solution solution=new Solution();
         solution.setZeroes(matrix);
+        int m=matrix.length;
         int n=matrix[0].length;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 System.out.print(matrix[i][j]+",");
             }
