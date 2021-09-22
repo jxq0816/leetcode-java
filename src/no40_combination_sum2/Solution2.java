b0858a5406e7518c960a3b5997607c1d9195568a
姜兴琪 <xingqijiang@qq.com>
2021-09-22 23:47:23 +0800

    No.40 组合总和 II（DFS）

---
+++ src/no40_combination_sum2/Solution2.java
@@ -0,0 +1,45 @@
+package no40_combination_sum2;
+
+import java.util.*;
+
+public class Solution2 {
+    List<List<Integer>> rs=new ArrayList<List<Integer>>();
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        if (candidates.length == 0) {
+            return rs;
+        }
+        Arrays.sort(candidates);
+        Deque<Integer> path = new ArrayDeque<>(candidates.length);
+        dfs(candidates,path,target,0);
+        return rs;
+    }
+    void dfs(int[] candidates, Deque<Integer> path, int target, int index){
+        if(target==0){
+            rs.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = index; i < candidates.length; i++) {
+            // 大剪枝：减去 candidates[i] 小于 0，减去后面的 candidates[i + 1]、candidates[i + 2] 肯定也小于 0，因此用 break
+            if (target - candidates[i] < 0) {
+                break;
+            }
+            // 小剪枝：同一层相同数值的结点，从第 2 个开始，候选数更少，结果一定发生重复，因此跳过，用 continue
+            if (i > index && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+            path.addLast(candidates[i]);
+            System.out.println("递归之前 => " + path + "，剩余 = " + (target - candidates[i]));
+            // 因为元素不可以重复使用，这里递归传递下去的是 i + 1 而不是 i
+            dfs(candidates,path,target-candidates[i],i+1);
+            path.removeLast();
+            System.out.println("递归之后 => " + path + "，剩余 = " + (target - candidates[i]));
+        }
+    }
+    public static void main(String[] args) {
+        int[] candidates = new int[]{10, 1, 2, 7, 6, 1, 5};
+        int target = 8;
+        Solution2 solution = new Solution2();
+        List<List<Integer>> res = solution.combinationSum2(candidates, target);
+        System.out.println("输出 => " + res);
+    }
+}
