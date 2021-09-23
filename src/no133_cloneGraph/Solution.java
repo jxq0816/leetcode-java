3522519fdf80f71d77277737cdc18f29273c45c6
姜兴琪 <xingqijiang@qq.com>
2021-09-23 20:57:24 +0800

    133. 克隆图

---
+++ src/no133_cloneGraph/Solution.java
@@ -0,0 +1,28 @@
+package no133_cloneGraph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+public class Solution {
+    HashMap<Node,Node> visited=new HashMap<Node,Node>();
+    public Node cloneGraph(Node node) {
+        if(node==null){
+            return null;
+        }
+        if(visited.containsKey(node)){
+            return visited.get(node);
+        }
+        Node newNode=new Node(node.val);
+        visited.put(node,newNode);
+        List<Node> newList=new ArrayList<>();
+        List<Node> list=node.neighbors;
+        newNode.neighbors=list;
+        for(Node nb:list){
+            newList.add(cloneGraph(nb));
+        }
+        newNode.neighbors=newList;
+        return newNode;
+    }
+}
