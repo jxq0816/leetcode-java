2e0775551ce0fa4b0cd41ef2bd947fa091dc5f8d
xingqi5 <xingqi5@staff.sina.com>
2024-05-10 10:03:21 +0800

    dfs

--- src/no39_combination_sum/Solution.java
+++ src/no39_combination_sum/Solution.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
-    List<List<Integer>> ans=new ArrayList<List<Integer>>();
-    List<Integer> combine=new ArrayList<Integer>();
+    List ans=new ArrayList<List<Integer>>();
+    List combine=new ArrayList<Integer>();
 
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public List combinationSum(int[] candidates, int target) {
         dfs(candidates,target,0);
         return ans;
     }
